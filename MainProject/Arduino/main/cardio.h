febfbabf4f2112c6b4594a897bab0bc13850915d
Benjamin Coudannes <33093409+RoseauFragile@users.noreply.github.com>
2017-11-20 16:20:50 +0100

    Add files via upload

---
+++ MainProject/Arduino/main/cardio.h
@@ -0,0 +1,9 @@
+#ifndef CARDIO_H
+#define CARDIO_H
+
+#include <arduino.h>
+
+int getPulse(int *value, int *valueLast, unsigned long *time1, unsigned long *timeLast);
+
+#endif
+
---
+++ MainProject/Arduino/main/main.ino
@@ -0,0 +1,18 @@
+#include "coeur.h"
+#include "cardio.h"
+
+unsigned long time1;
+unsigned long timeLast = 0;
+int value;
+int valueLast;
+
+void setup() {
+  ini();
+}
+
+void loop() {
+
+ getPulse(&value, &valueLast, &time1, &timeLast);
+ choix();
+
+}
